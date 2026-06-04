d3467372d4c07a6cd15ba4e61d6fca5ed576da12
WeyehnR <weyehn1@gmail.com>
2026-06-04 10:11:13 -0400

    Remove WeatherForecast template files

---
+++ PayRollAnalzyser/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace PayRollAnalzyser.Models;
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    //an employee can have multiple payroll records, so we need a collection to hold those records
+    public ICollection<Payroll> Payrolls { get; set; } = new List<Payroll>();
+
+}
---
+++ PayRollAnalzyser/Models/Payroll.cs
@@ -0,0 +1,50 @@
+﻿namespace PayRollAnalzyser.Models;
+
+public class Payroll
+{
+    private decimal _hoursWorked;
+    private decimal _hourlyRate;
+
+    //need a payroll id to link to distigush between different payroll records (possibly on the same day)
+    public int Id { get; init; }
+
+    //also need an employee id to link to employee
+    public int EmployeeId { get; set; }
+    public Employee Employee { get; set; } = null!;
+    public decimal HoursWorked
+    {
+        get => _hoursWorked;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(HoursWorked), "HoursWorked cannot be negative.");
+            }
+            else
+            { 
+                _hoursWorked = value;
+            }
+        }
+    }
+    public decimal HourlyRate
+    {
+        get => _hourlyRate;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(HourlyRate), "HourlyRate cannot be negative.");
+            }
+            else
+            {
+                _hourlyRate = value;
+            }
+        }
+    }
+
+    public decimal TotalGrossPay => CalculateSalary();
+    public decimal CalculateSalary()
+    {
+        return HoursWorked * HourlyRate;
+    }
+}
