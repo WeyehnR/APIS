118d063ef611541c4bd97610172e321d077619e5
WeyehnR <weyehn1@gmail.com>
2026-06-04 09:44:01 -0400

    opened this project in visual studio and add this one to track files

---
+++ PayrollPratice/Models/EmployeeOLD.cs
@@ -0,0 +1,33 @@
+﻿namespace PayrollPratice.Models;
+
+class EmployeeOLD
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; } = "";
+    public required decimal HourlyRate { get; set; }
+    public required string DepartmentName { get; set; }
+
+    public string CompanyName { get; set; } = "Microsoft";
+    //birth date
+    public required DateTime BirthDate { get; set; }
+
+    //start date
+    public required DateTime StartDate { get; set; }  
+    public required DateTime EndDate { get; set; }
+
+}
+
+/*
+ * Behind the scenes, the C# compiler generates a class that looks something like this:
+ * 
+ * class Employee
+ * {
+ *      private int _Id;
+ *      private string _Name;
+ *      private decimal _HourlyRate;
+ *      
+ *      public int Id { get { return _Id; } set { _Id = value; } }
+ *      public string Name { get { return _Name; } set { _Name = value; } }
+ *      public decimal HourlyRate { get { return _HourlyRate; } set { _HourlyRate = value; } }
+ * }
+ */
---
+++ PayrollPratice/ProgramOLD.cs
@@ -0,0 +1,7 @@
+﻿using PayrollPratice;
+
+var employee = new Employee { Id = 1, Name = "John Doe", HourlyRate = 15.50m };
+
+Console.WriteLine($"Employee: {employee.Name}");
+Console.WriteLine($"ID: {employee.Id}");
+Console.WriteLine($"Hourly Rate: ${employee.HourlyRate}");
